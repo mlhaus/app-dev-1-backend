8d5af551272616647f49f57175e05fea32f17408
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-09 21:31:53 -0500

    Port 5000

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ dbConnect();
 
 // Application setup
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // Body parser middleware
 app.use(express.json());
