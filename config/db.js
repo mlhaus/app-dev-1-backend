5b718af402f456e706b66f27d084af7acb13f133
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-09 22:25:54 -0500

    index.js to root

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 const connect = async () => {
   const conn = await mongoose.connect(process.env.MONGODB_URI_TODOS);
-    // console.log(`Connected to MongoDB connection: ${conn.connection.host}`);
+    console.log(`Connected to MongoDB connection: ${conn.connection.host}`);
 };
 
 module.exports = connect;
--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Application dependencies
 const express = require('express');
 require('dotenv').config();
-const dbConnect = require('../config/db.js');
+const dbConnect = require('./config/db.js');
 dbConnect();
 
 // Application setup
@@ -13,15 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Route definition
-// app.use(express.static('public')); // This will display /public/index.html when the server loads
+app.use(express.static('public')); // This will display /public/index.html when the server loads
 app.get("/", (request, response) => {
     response.send("Welcome to Marc's API!");
 });
 
-const todosRouter = require("../routes/todos");
+const todosRouter = require("./routes/todos");
 app.use("/api/todos", todosRouter);
 
-const postsRouter = require("../routes/posts");
+const postsRouter = require("./routes/posts");
 app.use("/api/posts", postsRouter);
 
 app.get("/test-err", (req, res) => {
