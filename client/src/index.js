cb767ef18e75ed30688fc5779f1dc5d28bd4c507
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-15 14:20:15 -0500

    April 15

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,35 @@
 const todoForm = document.getElementById('todo-form');
+const todosList = document.getElementById('todos');
+
+
+function showTodos() {
+    fetch('http://localhost:5000/api/todos', {
+        // fetch('https://nodejs25.vercel.app/api/todos', {
+        method: 'GET'
+    }).then(res => res.json())
+        .then(json => {
+            todosList.textContent = ''; // Clears the table body
+            const todos = json.data;
+            todos.forEach(todo => {
+                const tr = document.createElement('tr');
+
+                const col1 = document.createElement('td');
+                col1.textContent = todo.title;
+                tr.appendChild(col1);
+
+                const col2 = document.createElement('td');
+                col2.textContent = todo.completed;
+                tr.appendChild(col2);
+
+                const col3 = document.createElement('td');
+                col3.innerHTML = `<button data-id="${todo._id}">Mark completed</button>`;
+                tr.appendChild(col3);
+
+                todosList.appendChild(tr);
+            })
+        });
+
+}
 
 todoForm.addEventListener('submit', function(event) {
     event.preventDefault();
@@ -6,11 +37,12 @@ todoForm.addEventListener('submit', function(event) {
     let toDo;
     if(newToDo !== '') {
         toDo = {title: newToDo};
-
-        fetch('https://nodejs25.vercel.app/api/todos', {
+        fetch('http://localhost:5000/api/todos', {
+        // fetch('https://nodejs25.vercel.app/api/todos', {
             method: 'POST',
             body: JSON.stringify(toDo),
             headers: {'Content-Type': 'application/json; charset=UTF-8'}
-        }).then(res => console.log(res.json()));
+        }).then(res => showTodos);
     }
 });
+window.addEventListener('DOMContentLoaded', showTodos);
