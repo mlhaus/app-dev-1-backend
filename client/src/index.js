54cab764645ec74caeacd71a58ffe808ca917d20
Marc Hauschildt <marc.hauschildt@gmail.com>
2025-04-17 14:07:42 -0500

    April 17

--- client/src/index.js
+++ client/src/index.js
@@ -1,48 +1,210 @@
-const todoForm = document.getElementById('todo-form');
-const todosList = document.getElementById('todos');
+import './css/style.css';
+const itemForm = document.getElementById('item-form');
+const itemFormBtn = document.querySelector('#item-form button');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+const clearBtn = document.getElementById('clear');
+const filter = document.getElementById('filter');
 
+// Start create button functionality
+function createButton(textColor = 'black', iconName = '', ...classes) {
+    const button = document.createElement('button');
+    button.className = `btn-link text-${textColor}`;
+    classes.forEach(c => button.classList.add(c));
+    if(iconName !== '') {
+        const icon = createIcon(iconName);
+        button.appendChild(icon);
+    }
+    return button;
+}
+function createIcon(iconName) {
+    const icon = document.createElement('i');
+    icon.className = `fa-solid fa-${iconName}`;
+    return icon;
+}
+function createListItem(item) {
+    let listItem = document.createElement('li');
+    listItem.appendChild(document.createTextNode(item[0]));
+    listItem.setAttribute('data-id', item[1]);
+    const button = createButton('red', 'circle-xmark', 'remove-item');
+    listItem.appendChild(button);
+    itemList.appendChild(listItem);
+}
+// End create button functionality
 
-function showTodos() {
+// Start localStorage functionality
+function getItemsFromStorage() {
+    let listItemsArr = [];
     // fetch('http://localhost:5000/api/todos', {
-        fetch('https://nodejs25.vercel.app/api/todos', {
+    fetch('https://nodejs25.vercel.app/api/todos', {
         method: 'GET'
     }).then(res => res.json())
         .then(json => {
-            todosList.textContent = ''; // Clears the table body
             const todos = json.data;
             todos.forEach(todo => {
-                const tr = document.createElement('tr');
-
-                const col1 = document.createElement('td');
-                col1.textContent = todo.title;
-                tr.appendChild(col1);
-
-                const col2 = document.createElement('td');
-                col2.textContent = todo.completed;
-                tr.appendChild(col2);
-
-                const col3 = document.createElement('td');
-                col3.innerHTML = `<button data-id="${todo._id}">Mark completed</button>`;
-                tr.appendChild(col3);
-
-                todosList.appendChild(tr);
+                const title = todo.title;
+                const id = todo._id;
+                listItemsArr.push([title, id]);
             })
+        })
+        .then(function() {
+            // listItemsArr is a Two-dimensional array
+            // item is an array
+            listItemsArr.forEach(item => {
+                createListItem(item);
+            });
         });
-
 }
 
-todoForm.addEventListener('submit', function(event) {
-    event.preventDefault();
-    const newToDo = event.target.firstElementChild.value;
-    let toDo;
-    if(newToDo !== '') {
-        toDo = {title: newToDo};
+function storeListItem(itemName) {
+    if(itemName !== "") {
         // fetch('http://localhost:5000/api/todos', {
         fetch('https://nodejs25.vercel.app/api/todos', {
             method: 'POST',
-            body: JSON.stringify(toDo),
+            body: JSON.stringify({title: itemName}),
             headers: {'Content-Type': 'application/json; charset=UTF-8'}
-        }).then(res => showTodos);
+        }).then(res => res.json())
+            .then(json => {
+                if(json.success) {
+                    createListItem(itemName);
+                }
+            });
+    }
+}
+function setUp() {
+    itemList.innerHTML = '';
+    getItemsFromStorage();
+}
+function clearStorage() {
+    localStorage.removeItem('items');
+}
+// End localStorage functionality
+
+// Start Update/Delete functionality
+function updateItem(item) {
+    // Step 1: place the item's text in the form's input field
+    itemInput.value = item.textContent;
+    // Step 2: Change the Add button to an Update button
+    itemFormBtn.innerHTML = '<i class="fa-solid fa-pen"></i> Update Item';
+    itemFormBtn.style.backgroundColor = '#228B22';
+    // Step 3: Change the style of all buttons except the one that was clicked
+    itemList.querySelectorAll('li').forEach(i => i.classList.remove('edit-mode'));
+    item.classList.add('edit-mode');
+}
+function inEditMode() {
+    let editMode = false;
+    const listItems = itemList.querySelectorAll('li');
+    for (let i = 0; i < listItems.length; i++) {
+        if(listItems[i].classList.contains('edit-mode')) {
+            editMode = true;
+            break;
+        }
+    }
+    return editMode;
+}
+function updateListItem(itemName) {
+    const listItems = itemList.querySelectorAll('li');
+    for (let i = 0; i < listItems.length; i++) {
+        let currentItem = listItems[i];
+        if(currentItem.classList.contains('edit-mode')) {
+            const id = currentItem.getAttribute('data-id');
+            // Todo: validate the id
+            const toDo = {_id: id, title: itemName, userId: 1, completed: false};
+            // fetch('http://localhost:5000/api/todos/' + id, {
+            fetch('https://nodejs25.vercel.app/api/todos', {
+                method: 'PUT',
+                body: JSON.stringify(toDo),
+                headers: {'Content-Type': 'application/json; charset=UTF-8'}
+            }).then(res => res.json())
+                .then(json => {
+                    if(json.success) {
+                        currentItem.textContent = "";
+                        currentItem.appendChild(document.createTextNode(itemName));
+                        const button = createButton('red', 'circle-xmark', 'remove-item');
+                        currentItem.appendChild(button);
+                        turnOffEdit(currentItem);
+                    }
+                });
+            break;
+        }
+    }
+}
+function turnOffEdit(item) {
+    // Step 1: remove the text in the form's input field
+    itemInput.value = "";
+    // Step 2: Change the Update button to an Add button
+    itemFormBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+    itemFormBtn.style.backgroundColor = '#333';
+    // Step 3: Change the style of all buttons except the one that was clicked
+    item.classList.remove('edit-mode');
+}
+
+// End Update/Delete functionality
+
+// Start Event Listeners
+window.addEventListener('DOMContentLoaded', setUp);
+itemForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let editMode = inEditMode();
+    let inputItemValue = itemInput.value;
+    if(inputItemValue !== '') {
+        if (!editMode) {
+            // Adding a new item
+            storeListItem(inputItemValue);
+            itemInput.value = '';
+        } else {
+            // Edit an existing item
+            updateListItem(inputItemValue);
+        }
+    }
+})
+itemList.addEventListener('click', function (event) {
+    if (event.target.tagName === 'LI') {
+        // console.log("You clicked the list item");
+        if(event.target.classList.contains('edit-mode')) {
+            // Turn off edit mode
+            turnOffEdit(event.target);
+        } else {
+            // Turn on edit mode
+            updateItem(event.target);
+        }
+    } else if(event.target.parentElement.classList.contains('remove-item')) {
+        // console.log("You clicked the delete button");
+        // This is your homework
+        // Hint: Target the li, which is the parent of the button, which is the parent of the icon
     }
 });
-window.addEventListener('DOMContentLoaded', showTodos);
+clearBtn.addEventListener('click', function(event) {
+    let confirmClear = confirm('Are you sure you want to clear the list?');
+    if (confirmClear) {
+        itemList.innerHTML = '';
+        clearStorage();
+    }
+});
+// These events didn't work: change, keydown, keypress
+filter.addEventListener('input', (event) => {
+    let value = event.target.value;
+    const listItemsArr = getItemsFromStorage();
+    const filteredItems = listItemsArr.filter(item =>  item.toLowerCase().includes(value.toLowerCase()));
+    itemList.innerHTML = '';
+    filteredItems.forEach(item => {createListItem(item)});
+});
+// End Event Listeners
+
+// ****************************************
+// *      Start Code from February 27     *
+// ****************************************
+// listItems[0].style.backgroundColor = 'yellow';
+// listItems[1].style.backgroundColor = 'orange';
+// listItems.forEach((el) => el.style.color = 'red');
+//
+// const mainHeading = document.getElementById('main-heading');
+// mainHeading.textContent = "Marc's Shopping List"
+// mainHeading.style.color = 'yellow';
+// mainHeading.style.backgroundColor = 'dodgerblue';
+// mainHeading.style.padding = '0 20px';
+// mainHeading.style.borderRadius = '20px';
+// ****************************************
+// *      End Code from February 27       *
+// ****************************************
+
--- public/bundle.js
+++ public/bundle.js
@@ -7,26 +7,73 @@
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
 /******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/css/style.css":
+/*!***************************!*\
+  !*** ./src/css/style.css ***!
+  \***************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://client/./src/css/style.css?");
+
+/***/ }),
 
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("var todoForm = document.getElementById('todo-form');\nvar todosList = document.getElementById('todos');\nfunction showTodos() {\n  fetch('http://localhost:5000/api/todos', {\n    // fetch('https://nodejs25.vercel.app/api/todos', {\n    method: 'GET'\n  }).then(function (res) {\n    return res.json();\n  }).then(function (json) {\n    todosList.textContent = ''; // Clears the table body\n    var todos = json.data;\n    todos.forEach(function (todo) {\n      var tr = document.createElement('tr');\n      var col1 = document.createElement('td');\n      col1.textContent = todo.title;\n      tr.appendChild(col1);\n      var col2 = document.createElement('td');\n      col2.textContent = todo.completed;\n      tr.appendChild(col2);\n      var col3 = document.createElement('td');\n      col3.innerHTML = \"<button data-id=\\\"\".concat(todo._id, \"\\\">Mark completed</button>\");\n      tr.appendChild(col3);\n      todosList.appendChild(tr);\n    });\n  });\n}\ntodoForm.addEventListener('submit', function (event) {\n  event.preventDefault();\n  var newToDo = event.target.firstElementChild.value;\n  var toDo;\n  if (newToDo !== '') {\n    toDo = {\n      title: newToDo\n    };\n    fetch('http://localhost:5000/api/todos', {\n      // fetch('https://nodejs25.vercel.app/api/todos', {\n      method: 'POST',\n      body: JSON.stringify(toDo),\n      headers: {\n        'Content-Type': 'application/json; charset=UTF-8'\n      }\n    }).then(function (res) {\n      return showTodos;\n    });\n  }\n});\nwindow.addEventListener('DOMContentLoaded', showTodos);\n\n//# sourceURL=webpack://client/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./css/style.css */ \"./src/css/style.css\");\n\nvar itemForm = document.getElementById('item-form');\nvar itemFormBtn = document.querySelector('#item-form button');\nvar itemInput = document.getElementById('item-input');\nvar itemList = document.getElementById('item-list');\nvar clearBtn = document.getElementById('clear');\nvar filter = document.getElementById('filter');\n\n// Start create button functionality\nfunction createButton() {\n  var textColor = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'black';\n  var iconName = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : '';\n  var button = document.createElement('button');\n  button.className = \"btn-link text-\".concat(textColor);\n  for (var _len = arguments.length, classes = new Array(_len > 2 ? _len - 2 : 0), _key = 2; _key < _len; _key++) {\n    classes[_key - 2] = arguments[_key];\n  }\n  classes.forEach(function (c) {\n    return button.classList.add(c);\n  });\n  if (iconName !== '') {\n    var icon = createIcon(iconName);\n    button.appendChild(icon);\n  }\n  return button;\n}\nfunction createIcon(iconName) {\n  var icon = document.createElement('i');\n  icon.className = \"fa-solid fa-\".concat(iconName);\n  return icon;\n}\nfunction createListItem(item) {\n  var listItem = document.createElement('li');\n  listItem.appendChild(document.createTextNode(item[0]));\n  listItem.setAttribute('data-id', item[1]);\n  var button = createButton('red', 'circle-xmark', 'remove-item');\n  listItem.appendChild(button);\n  itemList.appendChild(listItem);\n}\n// End create button functionality\n\n// Start localStorage functionality\nfunction getItemsFromStorage() {\n  var listItemsArr = [];\n  // fetch('http://localhost:5000/api/todos', {\n  fetch('https://nodejs25.vercel.app/api/todos', {\n    method: 'GET'\n  }).then(function (res) {\n    return res.json();\n  }).then(function (json) {\n    var todos = json.data;\n    todos.forEach(function (todo) {\n      var title = todo.title;\n      var id = todo._id;\n      listItemsArr.push([title, id]);\n    });\n  }).then(function () {\n    // listItemsArr is a Two-dimensional array\n    // item is an array\n    listItemsArr.forEach(function (item) {\n      createListItem(item);\n    });\n  });\n}\nfunction storeListItem(itemName) {\n  if (itemName !== \"\") {\n    // fetch('http://localhost:5000/api/todos', {\n    fetch('https://nodejs25.vercel.app/api/todos', {\n      method: 'POST',\n      body: JSON.stringify({\n        title: itemName\n      }),\n      headers: {\n        'Content-Type': 'application/json; charset=UTF-8'\n      }\n    }).then(function (res) {\n      return res.json();\n    }).then(function (json) {\n      if (json.success) {\n        createListItem(itemName);\n      }\n    });\n  }\n}\nfunction setUp() {\n  itemList.innerHTML = '';\n  getItemsFromStorage();\n}\nfunction clearStorage() {\n  localStorage.removeItem('items');\n}\n// End localStorage functionality\n\n// Start Update/Delete functionality\nfunction updateItem(item) {\n  // Step 1: place the item's text in the form's input field\n  itemInput.value = item.textContent;\n  // Step 2: Change the Add button to an Update button\n  itemFormBtn.innerHTML = '<i class=\"fa-solid fa-pen\"></i> Update Item';\n  itemFormBtn.style.backgroundColor = '#228B22';\n  // Step 3: Change the style of all buttons except the one that was clicked\n  itemList.querySelectorAll('li').forEach(function (i) {\n    return i.classList.remove('edit-mode');\n  });\n  item.classList.add('edit-mode');\n}\nfunction inEditMode() {\n  var editMode = false;\n  var listItems = itemList.querySelectorAll('li');\n  for (var i = 0; i < listItems.length; i++) {\n    if (listItems[i].classList.contains('edit-mode')) {\n      editMode = true;\n      break;\n    }\n  }\n  return editMode;\n}\nfunction updateListItem(itemName) {\n  var listItems = itemList.querySelectorAll('li');\n  var _loop = function _loop() {\n    var currentItem = listItems[i];\n    if (currentItem.classList.contains('edit-mode')) {\n      var id = currentItem.getAttribute('data-id');\n      // Todo: validate the id\n      var toDo = {\n        _id: id,\n        title: itemName,\n        userId: 1,\n        completed: false\n      };\n      // fetch('http://localhost:5000/api/todos/' + id, {\n      fetch('https://nodejs25.vercel.app/api/todos', {\n        method: 'PUT',\n        body: JSON.stringify(toDo),\n        headers: {\n          'Content-Type': 'application/json; charset=UTF-8'\n        }\n      }).then(function (res) {\n        return res.json();\n      }).then(function (json) {\n        if (json.success) {\n          currentItem.textContent = \"\";\n          currentItem.appendChild(document.createTextNode(itemName));\n          var button = createButton('red', 'circle-xmark', 'remove-item');\n          currentItem.appendChild(button);\n          turnOffEdit(currentItem);\n        }\n      });\n      return 1; // break\n    }\n  };\n  for (var i = 0; i < listItems.length; i++) {\n    if (_loop()) break;\n  }\n}\nfunction turnOffEdit(item) {\n  // Step 1: remove the text in the form's input field\n  itemInput.value = \"\";\n  // Step 2: Change the Update button to an Add button\n  itemFormBtn.innerHTML = '<i class=\"fa-solid fa-plus\"></i> Add Item';\n  itemFormBtn.style.backgroundColor = '#333';\n  // Step 3: Change the style of all buttons except the one that was clicked\n  item.classList.remove('edit-mode');\n}\n\n// End Update/Delete functionality\n\n// Start Event Listeners\nwindow.addEventListener('DOMContentLoaded', setUp);\nitemForm.addEventListener('submit', function (event) {\n  event.preventDefault();\n  var editMode = inEditMode();\n  var inputItemValue = itemInput.value;\n  if (inputItemValue !== '') {\n    if (!editMode) {\n      // Adding a new item\n      storeListItem(inputItemValue);\n      itemInput.value = '';\n    } else {\n      // Edit an existing item\n      updateListItem(inputItemValue);\n    }\n  }\n});\nitemList.addEventListener('click', function (event) {\n  if (event.target.tagName === 'LI') {\n    // console.log(\"You clicked the list item\");\n    if (event.target.classList.contains('edit-mode')) {\n      // Turn off edit mode\n      turnOffEdit(event.target);\n    } else {\n      // Turn on edit mode\n      updateItem(event.target);\n    }\n  } else if (event.target.parentElement.classList.contains('remove-item')) {\n    // console.log(\"You clicked the delete button\");\n    // This is your homework\n    // Hint: Target the li, which is the parent of the button, which is the parent of the icon\n  }\n});\nclearBtn.addEventListener('click', function (event) {\n  var confirmClear = confirm('Are you sure you want to clear the list?');\n  if (confirmClear) {\n    itemList.innerHTML = '';\n    clearStorage();\n  }\n});\n// These events didn't work: change, keydown, keypress\nfilter.addEventListener('input', function (event) {\n  var value = event.target.value;\n  var listItemsArr = getItemsFromStorage();\n  var filteredItems = listItemsArr.filter(function (item) {\n    return item.toLowerCase().includes(value.toLowerCase());\n  });\n  itemList.innerHTML = '';\n  filteredItems.forEach(function (item) {\n    createListItem(item);\n  });\n});\n// End Event Listeners\n\n// ****************************************\n// *      Start Code from February 27     *\n// ****************************************\n// listItems[0].style.backgroundColor = 'yellow';\n// listItems[1].style.backgroundColor = 'orange';\n// listItems.forEach((el) => el.style.color = 'red');\n//\n// const mainHeading = document.getElementById('main-heading');\n// mainHeading.textContent = \"Marc's Shopping List\"\n// mainHeading.style.color = 'yellow';\n// mainHeading.style.backgroundColor = 'dodgerblue';\n// mainHeading.style.padding = '0 20px';\n// mainHeading.style.borderRadius = '20px';\n// ****************************************\n// *      End Code from February 27       *\n// ****************************************\n\n//# sourceURL=webpack://client/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
